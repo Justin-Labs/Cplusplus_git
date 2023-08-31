d6a4bfb8370589b90f9d9f074ba0206abf1e2b1f
justin morton <nukeshadows@gmail.com>
2023-08-31 08:04:50 -0700

    commented the one method

--- Using Inheritance for is -a Relationships/Using Public Inheritance/v01/GraduateStudent.cpp
+++ Using Inheritance for is -a Relationships/Using Public Inheritance/v01/GraduateStudent.cpp
@@ -7,6 +7,6 @@ using std::string;
 using std::cout;
 using std::endl;
 
-bool GraduateStudent::has_scholarship() const {
+bool GraduateStudent::has_scholarship() const {         //one method has_scholarship() which is marked const and simply returns member variable scholarhip
     return scholarship;
 }
