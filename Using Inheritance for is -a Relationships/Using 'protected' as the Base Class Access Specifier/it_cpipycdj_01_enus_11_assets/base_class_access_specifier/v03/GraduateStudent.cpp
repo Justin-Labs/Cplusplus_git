0bfce4dc428fb54197f2e15f2645a5fe78850486
justin morton <nukeshadows@gmail.com>
2023-08-31 12:39:42 -0700

    Restored files from last group file delete, wrong tutorial

---
+++ Using Inheritance for is -a Relationships/Using 'protected' as the Base Class Access Specifier/it_cpipycdj_01_enus_11_assets/base_class_access_specifier/v03/GraduateStudent.cpp
@@ -0,0 +1,24 @@
+#include <string>
+#include <iostream>
+
+#include "GraduateStudent.h"
+
+using std::string;
+using std::cout;
+using std::endl;
+
+bool GraduateStudent::has_scholarship() const {
+    return scholarship;
+}
+
+void GraduateStudent::set_scholarship(bool s) {
+    scholarship = s;
+}
+
+void GraduateStudent::display_info()  const {
+    // cout << "Name: " << name << endl;
+    cout << "GPA: " << gpa << endl;
+    cout << "Major: " << major << endl;
+
+    cout << "Has scholarship: " << std::boolalpha << scholarship << endl;
+}
